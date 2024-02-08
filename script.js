3c5dc2c82b258f518c707f24f92e85189e22813f
andicretu <149256225+andicretu@users.noreply.github.com>
2024-02-08 08:16:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,96 @@
+let inputWord;
+let newWord;
+let newDef;
+
+function readInput() {
+  const inputElement = document.getElementById('userInput');
+  const inputText = inputElement.value;
+  inputWord = inputText;
+}
+
+var dict = {
+  "dictionar": "a book or electronic resource that lists the words of a language...",
+  "key": "a small piece of shaped metal with incisions cut to fit the wards of a particular lock..."
+};
+
+  function checkInputWord() {
+    const buttonContainer = document.getElementById("buttonContainer");
+    const buttonHTMLNewWord = `
+  <br>
+  <div>
+  <button type="button" class="btn btn-primary" id="button1">NEW WORD</button>
+  </div>
+  <br>`;
+  const buttonHTMLExistingWord = `
+  <br>
+  <div>
+  <button type="button" class="btn btn-primary" id="button1">EXISTING WORD: </button>
+  </div>
+  <br>`;
+
+  if (dict.hasOwnProperty(inputWord)) {
+    buttonContainer.innerHTML = buttonHTMLExistingWord;
+    showWordDefinitionDict(inputWord);
+  } else if (localStorage.getItem(inputWord)) {
+    buttonContainer.innerHTML = buttonHTMLExistingWord;
+    showWordDefinitionLocal(inputWord);
+  } else {
+    buttonContainer.innerHTML = buttonHTMLNewWord;
+    showNewWordContainer();
+  } 
+}
+
+function showWordDefinitionLocal() {
+  const wordDefContainer = document.getElementById("textContainer");
+  const wordExplanation = localStorage.getItem(`definition_${inputWord}`);
+  const fiedWordExplanation = `
+<div class="card">
+<div class="card-body">
+  <p><em>${(wordExplanation)}</em></p>
+  </div>
+</div>`;
+  wordDefContainer.innerHTML = fiedWordExplanation;
+}
+
+function showWordDefinitionDict() {
+  const wordDefContainer = document.getElementById("textContainer");
+  const wordExplanation = dict[inputWord];
+  const fiedWordExplanation = `
+<div class="card">
+<div class="card-body">
+  <p><em>${(wordExplanation)}</em></p>
+  </div>
+</div>`;
+  wordDefContainer.innerHTML = fiedWordExplanation;
+}
+
+function showNewWordContainer() {
+  const wordDefContainer = document.getElementById("textContainer");
+  const fiedNewDefinition = `
+<div class="input-group mb-3">
+  <button class="btn btn-outline-primary" type="button" id="button-addon2">Save New word definition</button>
+  <input type="text" class="form-control" placeholder="Please input new word definition" aria-label="Example text with button addon" aria-describedby="button-addon2" id="userDefinition">
+</div>`;
+  wordDefContainer.innerHTML = fiedNewDefinition;
+  newWord = inputWord;
+}
+
+function readNewWordDefinition() {
+  const defElement = document.getElementById('userDefinition');
+  const defText = defElement.value;
+  newDef = defText;
+}
+
+function saveNewWordDefinition() {
+  readNewWordDefinition();
+  localStorage.setItem(`definition_${newWord}`, newDef);
+}
+
+  document.getElementById('button-addon1').addEventListener('click', function() {
+    readInput()
+    checkInputWord()
+  });
+
+  document.getElementById('button-addon2').addEventListener('click', function() {
+    saveNewWordDefinition();
+  })
